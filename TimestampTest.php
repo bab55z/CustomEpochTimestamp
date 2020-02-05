08682eb93621d413f2bb66c9c0c35304de65206c
Williem Babalola <bab55z@yahoo.fr>
2020-02-05 13:50:31 +0000

    Update TimestampTest.php

--- TimestampTest.php
+++ TimestampTest.php
@@ -2,14 +2,21 @@
 
 class BaseTest extends TestCase
 {
-    /**
+   /**
      * Test.
      *
      * @return void
      */
     public function testCustomTimestamp()
     {
-        //var_dump((new Timestamp)());
         $this->assertIsInt((new Timestamp)());
+    }
+
+    public function testCustomTimestampString()
+    {
+        var_dump((new Timestamp)());
+        $this->assertIsInt((new Timestamp)());
+        $timestampString = '' . new Timestamp;
+        $this->assertIsString($timestampString);
     }
 }
