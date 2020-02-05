c383d98adde394c3dd529b5722a1c7ca52832c0a
Williem Babalola <bab55z@yahoo.fr>
2020-02-05 12:50:40 +0000

    Update Timestamp.php

--- Timestamp.php
+++ Timestamp.php
@@ -2,14 +2,21 @@
 
 class timestamp {
 
-  /**
-   * returns a timestamp with custom epoch, to get shorter unique timestamp
-   */
-   
-  public function __invoke() : int {
-    $customStartEpoch = strtotime('2020-01-01 00:00:00'); // custom epoch
-    $now = strtotime(date('Y-m-d H:i:s'));
-    return (int) ($now - $customStartEpoch);
-  }
+    /**
+     * returns a timestamp with custom epoch, to get shorter unique timestamp
+     */
+
+    protected $timestamp;
+
+    public function __construct()
+    {
+        $customStartEpoch = strtotime('2020-01-01 00:00:00'); // custom epoch
+        $now = strtotime(date('Y-m-d H:i:s'));
+        $this->timestamp = $now - $customStartEpoch;
+    }
+
+    public function __invoke() : int {return (int) $this->timestamp;}
+    public function __toString(){return (string) ($this->timestamp);}
+
   
 }
